9d508426c38b74f7648a41e1068a13914a7aaf3a
joao1-barbosa <joao.rbarbosa3@gmail.com>
2022-12-11 00:07:46 -0300

    file handling

---
+++ src/middlewares/uploadImage.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      const uploadDir = process.cwd() + '/public/upload';
+      if (!fs.existsSync(uploadDir)) {
+        // if not create directory
+        fs.mkdirSync(uploadDir);
+      }
+      cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(
+        null,
+         'image'+
+        '-' +
+          uniqueSuffix +
+          path.extname(file.originalname)
+      );
+    },
+});
+
+module.exports = multer({
+    storage: storage,
+});
